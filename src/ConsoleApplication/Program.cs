bc8e7cf12cf5764cf578a562b8ada17665b5a91f
Jeff Kluge <jeffkl@microsoft.com>
2018-01-26 10:42:34 -0800

    Initial commit

---
+++ src/ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Assembly assembly1 = AssemblyLoadContext.Default.LoadFromAssemblyPath(@"D:\msbuild\bin\Bootstrap-NetCore\NuGet.Versioning.dll");
+            Assembly assembly2 = System.Runtime.Loader.AssemblyLoadContext.Default.LoadFromAssemblyPath(@"D:\msbuild\bin\Bootstrap-NetCore\NuGet.Versioning.dll");
+        }
+    }
+}
